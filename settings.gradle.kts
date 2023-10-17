8cb43548f35abec93c80daf744243d3ca9fc2199
Mehdi Aouragh <2002081000378@ofppt-edu.ma>
2023-10-17 17:35:09 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DEV MOB 201"
+include(":app")
+ 
